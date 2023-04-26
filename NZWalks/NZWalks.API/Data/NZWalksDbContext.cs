c8333e18baf057633912bd01abcce1577dbd9d29
pamee28 <pamela.suazo@puzzle.tech>
2023-04-26 17:57:19 -0600

    Seccion 1-2

---
+++ NZWalks/NZWalks.API/Data/NZWalksDbContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Conventions;
+using NZWalks.API.Models.Domain;
+using System.Collections.Generic;
+
+namespace NZWalks.API.Data
+{
+    public class NZWalksDbContext:DbContext
+    {
+        public NZWalksDbContext(DbContextOptions dbContextOptions) : base(dbContextOptions)
+        {
+
+        }
+        
+        public DbSet<Difficulty> Difficulties { get; set; }
+        public DbSet<Region> Regions{ get; set; }
+        public DbSet<Walk> Walks { get; set; }
+    }
+
+}
+
+ 
